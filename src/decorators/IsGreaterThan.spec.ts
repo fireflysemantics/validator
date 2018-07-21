2909517c65e06851e9b6a8d02bc0abb27b112df8
Ole <ole.ersoy@gmail.com>
2018-07-20 20:40:30 -0500

    Added more IsGreaterThan tests

--- src/decorators/IsGreaterThan.spec.ts
+++ src/decorators/IsGreaterThan.spec.ts
@@ -24,10 +24,32 @@ class IsGreaterThanTest2 {
 
 const IGTT2 = new IsGreaterThanTest2();
 
+class IsGreaterThanTest3 {
+  firstNumber: number = 20; 
+
+  @IsGreaterThan("firstNumber")
+  secondNumber: number = 10;
+}
+
+const IGTT3 = new IsGreaterThanTest3();
+
+class IsGreaterThanTest4 {
+  firstNumber: number = 10; 
+
+  @IsGreaterThan("firstNumber")
+  secondNumber: number = 10;
+}
+
+const IGTT4 = new IsGreaterThanTest4();
+
 describe("IfGreaterThan Validation", () => {
   it("should work for string property key arguments", () => {
     expect(validateProperty(IGTT1, "secondNumber")).to.be.true;
     expect(validate(IGTT1)).to.be.true;
+    expect(validateProperty(IGTT3, "secondNumber")).to.be.false;
+    expect(validate(IGTT3)).to.be.false;
+    expect(validateProperty(IGTT4, "secondNumber")).to.be.false;
+    expect(validate(IGTT4)).to.be.false;
   });
   it("should work for number property key arguments", () => {
     expect(validateProperty(IGTT2, "secondNumber")).to.be.true;
