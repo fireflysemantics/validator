b9c81875f72469e38c81cf8591178cc0c15e17ba
Ole Ersoy <ole.ersoy@gmail.com>
2019-06-12 18:16:59 -0500

    Switched index.ts to absolute paths

--- src/container/validation/index.ts
+++ src/container/validation/index.ts
@@ -1,4 +1,4 @@
-export { MetaClass } from "./MetaClass";
-export { ValidationContainer } from "./ValidationContainer";
-export { ValidationContext } from "./ValidationContext";
-export { ValidationOptions } from "./ValidationOptions";
+export { MetaClass } from "@fs/container/validation/MetaClass";
+export { ValidationContainer } from "@fs/container/validation/ValidationContainer";
+export { ValidationContext } from "@fs/container/validation/ValidationContext";
+export { ValidationOptions } from "@fs/container/validation/ValidationOptions";
